fc79d262df64683001476789bf17158f01329ce4
Metakino <68518066+Metakino@users.noreply.github.com>
2020-08-09 14:18:37 +0200

    Added Script info + Update version

--- AMQUpdatehotkey.user.js
+++ AMQUpdatehotkey.user.js
@@ -1,12 +1,16 @@
 // ==UserScript==
 // @name         AMQ Update hotkey
 // @namespace    https://github.com/Metakino
-// @version      0.1
+// @version      1.1
 // @description  Add an Hotkey to update your MAL list automatically
 // @author       Metakino
 // @match        https://animemusicquiz.com/*
+// @require      https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqScriptInfo.js
 // @grant        none
 // ==/UserScript==
+
+
+if (!window.setupDocumentDone) return;
 
 function key_up(event) {
     if(event.altKey && event.keyCode==82) {
@@ -14,9 +18,27 @@ function key_up(event) {
 	} else if(event.altKey && event.keyCode==84){
         options.updateKitsu();
     }else if(event.altKey && event.keyCode==89){
+        console.log(lobby.soloMode);
         options.updateMal();
     }
-
 };
 
 document.addEventListener('keyup', key_up, false);
+
+
+/** --- script info --- **/
+
+
+// Add metadata
+AMQ_addScriptData({
+    name: "Update hotkey",
+    author: "Metakino",
+    description: `
+        <p>You can update your lists quickly without having to go in the settings with hotkeys : </p>
+        <table>
+           <tr><td>• Anilist : <b>[ALT + R]</b></td></tr>
+           <tr><td>• Kitsu : <b>[ALT + T]</b></td></tr>
+           <tr><td>• Anilist : <b>[ALT + Y]</b></td></tr>
+        </table>
+    `
+});

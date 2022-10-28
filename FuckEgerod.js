6ecd6ca655cf6fb6e1734db0ee37479eb2227ab9
Metakino <68518066+Metakino@users.noreply.github.com>
2022-10-28 20:35:29 +0200

    Update FuckEgerod.js

--- FuckEgerod.js
+++ FuckEgerod.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Fuck Egerod
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/Metakino
 // @version      AMQ Lazer
 // @description  Big Update When
 // @author       Metakino
-// @match        https://animemusicquiz.com/
+// @match        https://animemusicquiz.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=animemusicquiz.com
 // @grant        none
 // ==/UserScript==
